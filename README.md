77876c4c6ff6b54b9280b6e5150836fff723f980
thekindlyone <dodo.dodder@gmail.com>
2014-12-12 16:46:14 +0530

    readme

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 #IMGURU
 
 Pushes images to imgur from command line.
+
+Wrote this so that I can easily share gui screenshots with my client for aesthetic inputs and use the same screenshots to troubleshoot with other programmers(people who actually know css  )
+It works like:
+```
+thekindlyone@deepthought:~/Pictures/Wallpapers$ imguru.py .
+directory detected 
+Uploaded Ubuntu (1).jpg to http://i.imgur.com/VBZMmK7.jpg
+Uploaded ubuntu-wallpaper-40666-41617-hd-wallpapers.jpg to http://i.imgur.com/j0RiAdp.jpg
+Uploaded ubuntu-wallpapers-21.png to http://i.imgur.com/7Rx58My.jpg
+Uploaded 1613386.jpg to http://i.imgur.com/o9Vn5nl.jpg
+Uploaded ubuntu-4188-1600x900.jpg to http://i.imgur.com/U9uVeqT.jpg
+Album Created! title: Wallpapers url: http://thekindlyone.co.in/imguru/gallery/ly4zJ6
+
+```
+Once the upload is finished, all the links are sent to a server where an album/gallery is generated with a permalink(this is to bypass the 2 factor authentication needed from imgur to create imgur album..something that is hard to arrange from a terminal)
+terminal app:
+https://github.com/thekindlyone/imguru
+serverside:
+https://github.com/thekindlyone/imguru_www
+example gallery:
+http://thekindlyone.co.in/imguru/gallery/ly4zJ6
--- imguru.py
+++ imguru.py
@@ -1,74 +1,79 @@
 #!/usr/bin/env python
 from __future__ import print_function
 from imgurpython import ImgurClient as imgur
-import webbrowser, os.path
+import webbrowser
+import os.path
 import sys
 import requests
 import json
 from imgurpython.helpers.error import ImgurClientError
 client_id = '0ee25e395ec9569'
 client_secret = '400182ea3b3818efcaae28d8614fef3df2713400'
-log= 'imguru.log'
-SUPPORTED=['.png','.jpg','.jpeg','.gif']
-def connect(client_id,client_secret):
+log = 'imguru.log'
+SUPPORTED = ['.png', '.jpg', '.jpeg', '.gif']
+
+
+def connect(client_id, client_secret):
     try:
-        client= imgur(client_id,client_secret)
-        return True,client
+        client = imgur(client_id, client_secret)
+        return True, client
     except ImgurClientError as e:
-        return False,e.error_message
+        return False, e.error_message
     except:
-        return False,sys.exc_info()[0]
+        return False, sys.exc_info()[0]
 
-def file_upload(client,filename):
+
+def file_upload(client, filename):
     try:
-        response=client.upload_from_path(filename)
-        link=response.get('link',False)
-        return True,response,filename
+        response = client.upload_from_path(filename)
+        link = response.get('link', False)
+        return True, response, filename
     except ImgurClientError as e:
-        return False,e.error_message,filename
+        return False, e.error_message, filename
     except:
-        return False,sys.exc_info()[0],filename
+        return False, sys.exc_info()[0], filename
 
 
-def dir_upload(client,path):
-    for root,dirs,files in os.walk(path):
+def dir_upload(client, path):
+    for root, dirs, files in os.walk(path):
         for fname in files:
             if os.path.splitext(fname)[-1].lower() in SUPPORTED:
-                yield file_upload(client,os.path.join(root,fname))
+                yield file_upload(client, os.path.join(root, fname))
 
 
 def make_album(**kwargs):
-    payload={'data':kwargs}
-    url='http://thekindlyone.co.in/imguru/create'
+    payload = {'data': kwargs}
+    url = 'http://thekindlyone.co.in/imguru/create'
     try:
-        r=requests.post(url,data=json.dumps(payload))
-        if r.status_code==200:
-            return True,r.content
-        else: 
-            return False,r.status_code       
+        r = requests.post(url, data=json.dumps(payload))
+        if r.status_code == 200:
+            return True, r.content
+        else:
+            return False, r.status_code
     except:
-        return False,sys.exc_info()[0]
+        return False, sys.exc_info()[0]
 
 
-def main():    
-    path=os.path.expanduser(sys.argv[1])
+def main():
+    path = os.path.expanduser(sys.argv[1])
     if not os.path.exists(path):
         print(" ERROR: {} not found.".format(path))
         sys.exit(1)
 
-    ok,response=connect(client_id,client_secret)
+    ok, response = connect(client_id, client_secret)
     if ok:
-        client=response
+        client = response
     else:
         print("cannot connect")
         sys.exit(1)
-    
-    if os.path.isdir(path): # entire directory
+
+    if os.path.isdir(path):  # entire directory
         print ('directory detected ')
-        title=os.path.split(path)[-1] if path != '.' else os.path.split(os.getcwd())[-1]
-        ids=[]
-        links=[]
-        for ok,result,filename in dir_upload(client,path):            
+        title = os.path.split(
+            path)[-1] if path != '.' else os.path.split(os.getcwd())[-1]
+        ids = []
+        links = []
+        for ok, result, filename in dir_upload(client, path):
             if ok:
                 ids.append(result.get('id'))
                 links.append(result.get('link'))
@@ -77,18 +82,18 @@ def main():
                     result.get('link')))
             else:
                 print('Error uploading {}. Info: {}'.format(
-                    filename,result))
-        
-        ok,content=make_album(title=title,links=links)
+                    filename, result))
+
+        ok, content = make_album(title=title, links=links)
         if ok:
             print ("Album Created! title: {} url: {}".format(
                 title,
                 content))
             webbrowser.open(content)
         else:
-            print("Cannot create album. Info: {}".format(content))                
-    else: # single file
-        ok,response,_=file_upload(client,path)
+            print("Cannot create album. Info: {}".format(content))
+    else:  # single file
+        ok, response, _ = file_upload(client, path)
         if ok:
             print("Uploaded {} to {}".format(
                 os.path.basename(path),
@@ -98,11 +103,6 @@ def main():
             print("Error uploading {}  Info: {}".format(
                 path,
                 response))
-
-
-
-
-
 
 
 if __name__ == '__main__':
